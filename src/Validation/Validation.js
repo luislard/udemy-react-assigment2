4368d5b1f808beaf17893ce0794af56bbbe8accb
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-07 00:18:28 +0100

    task 3 finished

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -4,6 +4,7 @@ const validation = (props) => {
     return (
       <div className="Validation">
         <p>{props.length}</p>
+        <p>{props.length > 5 ? 'Text too long...' : 'Text too short...'}</p>
       </div>
     )
   };
