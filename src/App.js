55de579565cef7145dbd7242e476457620e2abfc
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-07 23:49:55 +0100

    task 6 completed

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ class App extends Component {
     this.setState({ count: textCount, text: newText });
   }
 
+  deleteCharHandler = (charIndex) => {
+    
+    // const persons = this.state.persons; // this causes a flaw because we are pointing to the original state
+    // const persons = this.state.persons.slice(); // this is a better solution because it copies the original state array
+    const chars = [...this.state.text.slice('')]; // this is almost the same as before but with more modern syntax
+    chars.splice(charIndex, 1); 
+    this.setState({text: chars.join('')});
+  }
+
   render() {
 
     // prefered way to toggle persons
@@ -24,11 +33,14 @@ class App extends Component {
 
     if (this.state.text.length > 0) {
       let textArray = this.state.text.split('');
-      
+
       chars = (
         <div>
           {textArray.map((char, index) => {
-            return <CharComponent key={index} char={char} />
+            return <CharComponent 
+              click={this.deleteCharHandler.bind(this, index)}
+              key={index} 
+              char={char} />
           })}
         </div>
       );
@@ -36,7 +48,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input type="text" onChange={this.inputChangedHandler}/>
+        <input type="text" onChange={this.inputChangedHandler} value={this.state.text}/>
         <ValidationComponent length={this.state.count} />
         {chars}
       </div>
