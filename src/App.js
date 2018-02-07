9758fb331b4a614223122f04d65d89e094284004
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-07 01:17:19 +0100

    task 5 completed

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ import CharComponent from './CharComponent/CharComponent';
 class App extends Component {
 
   state = {
-    count: 0
+    count: 0,
+    text: ''
   }
 
   inputChangedHandler = (event) => {
-    const textCount = event.target.value.length;
-    this.setState({ count: textCount });
+    const newText = event.target.value;
+    const textCount = newText.length;
+    this.setState({ count: textCount, text: newText });
   }
 
   render() {
+
+    // prefered way to toggle persons
+    let chars = null;
+
+    if (this.state.text.length > 0) {
+      let textArray = [];
+      for (let index = 0; index < this.state.text.length; index++) {
+        textArray[index] = this.state.text.charAt(index);
+      }
+      chars = (
+        <div>
+          {textArray.map((char, index) => {
+            return <CharComponent key={index} char={char} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <input type="text" onChange={this.inputChangedHandler}/>
         <ValidationComponent length={this.state.count} />
-        <CharComponent length={this.state.count} />
+        {chars}
       </div>
     );
   }
--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -4,8 +4,7 @@ import './CharComponent.css';
 const charComponent = (props) => {
     return (
       <div className="CharComponent">
-        <p>{props.length}</p>
-        <p>{props.length > 5 ? 'Text too long...' : 'Text too short...'}</p>
+        <p>{props.char}</p>
       </div>
     )
   };
